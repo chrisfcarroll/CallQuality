73e61e47c54e659a19ca0f5e56273f4f89927886
Chris.Carroll <chris.carroll@unforgettable.me.uk>
2018-03-05 21:13:35 +0000

    netstandard20

--- NfrInvoke.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NFRInvoke.Tests")]
-[assembly: Guid("bcee3137-1072-4ccc-a5be-fa8d068a52e8")]
--- NfrInvoke/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("NFRInvoke")]
-[assembly: Guid("aca212fe-0148-4df3-af03-bba9ecb05d59")]
